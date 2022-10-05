e5fa2d30c48883b5933746e75269708e2ca0a122
rogxo <1910500309@qq.com>
2022-10-05 20:07:55 +0800

    Some very useful functions implementation in Windows Kernel

---
+++ WindowsKernelUtility/entry.cpp
@@ -0,0 +1,16 @@
+#include "includes.h"
+#include "Utils.hpp"
+#include "Imports.h"
+
+
+EXTERN_C NTSTATUS DriverUnload(PDRIVER_OBJECT Driver)
+{
+	UNREFERENCED_PARAMETER(Driver);
+	return STATUS_SUCCESS;
+}
+
+EXTERN_C NTSTATUS DriverEntry(PDRIVER_OBJECT Driver)
+{
+	KdPrint(("DriverEntry\n"));
+	return STATUS_SUCCESS;
+}
